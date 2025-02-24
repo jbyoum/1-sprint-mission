9025be462fd8552989846b14f886dcd095cccd3f
jbyoum <jbyoum3@gmail.com>
2025-02-24 18:55:57 +0900

    copy

---
+++ prisma/mock.js
@@ -0,0 +1,104 @@
+export const PRODUCTS = [
+  {
+    id: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+    name: "중고거래1",
+    description: "감동의 상품",
+    price: 10000,
+    createdAt: "2023-07-16T09:00:00Z",
+    updatedAt: "2023-07-16T09:00:00Z",
+  },
+  {
+    id: "6c3a18b0-11c5-4d97-9019-9ebe3c4d1317",
+    name: "중고거래2",
+    price: 10000,
+    tag: "상품태그",
+    createdAt: "2023-07-16T09:00:00Z",
+    updatedAt: "2023-07-16T09:00:00Z",
+  },
+];
+
+export const ARTICLES = [
+  {
+    id: "b8f11e76-0a9e-4b3f-bccf-8d9b4fbf331e",
+    title: "감동실화",
+    content: "진짜실화",
+    createdAt: "2023-07-16T09:00:00Z",
+    updatedAt: "2023-07-16T09:00:00Z",
+  },
+  {
+    id: "e1c1e5c1-5312-4f7b-a3d6-4cbb2b4f8828",
+    title: "게시글2",
+    content: "글내용2",
+    createdAt: "2023-07-16T09:00:00Z",
+    updatedAt: "2023-07-16T09:00:00Z",
+  },
+];
+
+export const PRODUCTCOMMENTS = [
+  {
+    id: "cad3b6ca-006c-4c7c-86c9-356b784f3500",
+    content: "실화1",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "e703839c-27dd-4c16-a054-81203ec88893",
+    content: "실화2",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "92d563f7-1af4-4f7f-aeed-3025c1eaaf0c",
+    content: "실화3",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "03b7ed75-7647-4d1b-a78e-4804a78bdb3f",
+    content: "실화4",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "bf9be335-255d-4e99-9da2-865bb49d0b65",
+    content: "실화5",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "09225a16-7143-4aa9-a2f0-2ada4a3ccb27",
+    content: "실화6",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "d1f978e9-7998-496e-93f1-b4f78fe781e2",
+    content: "실화7",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "b5f40c7c-498b-4a25-aec8-ae0302ffdfb8",
+    content: "실화8",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "4c879f82-26aa-46d8-a365-af8390d3579d",
+    content: "실화9",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "7a13d8c0-9017-464e-8bfa-8b42e95f89f3",
+    content: "실화10",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+  {
+    id: "4b52480b-d6f7-42ff-97d3-ba473cd95101",
+    content: "실화11",
+    createdAt: "2023-07-16T09:00:00Z",
+    productId: "936f5ea4-6e6c-4e5e-91a3-78f5644e1f9a",
+  },
+];
